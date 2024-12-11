295fe1c90d9077038bdaa1a1aeeadf465fb9a2c1
MogesBekele <mogesbekele32@gmail.com>
2024-12-11 17:09:07 +0300

    commited

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,7 +8,7 @@ const Testimonials = () => {
       id="Testimonials"
     >
       <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
-        Customer{" "}
+        Customer
         <span className="underline underline-offset-4 decoration-1 under font-light">
           Testimonials
         </span>
